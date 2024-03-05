16f5eb0977e1b83bd77fea0cc278b9d727240871
MapCreator <foryoumem@gmail.com>
2024-03-05 23:44:14 +0900

    버튼 유형에 따라 이벤트 함수 분리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import "./App.css"
 
-function getCalculateResult(num1, num2, operator)
+function isOperator(string)
+{
+  if (string === "+" || string === "-" || string === "*" || string === "/")
+    return true
+  else
+    return false
+}
+
+function getCalculatedResult(num1, num2, operator)
 {
   if (operator === "+")
   {
@@ -27,98 +35,87 @@ function getCalculateResult(num1, num2, operator)
 
 function App()
 {
-  let [currentButtonValue, setCurrentButtonValue] = useState("")
+  let [currentScreenValue, setCurrentScreenValue] = useState("")
   let [prevNumber, setPrevNumber] = useState("")
   let [operator, setOperator] = useState("")
 
-  function isEqual()
+  // 계산기에서 연산자(+, -, *, /) 버튼을 눌렀을 때
+  const operatorButtonEvent = (event) =>
   {
-    if (currentButtonValue !== "" && prevNumber !== "" && operator !== "")
-      {
-        const num1 = parseInt(prevNumber)
-        const num2 = parseInt(currentButtonValue)
-        const result = getCalculateResult(num1, num2, operator)
+    const buttonValue = event.target.name
 
-        setCurrentButtonValue(result)
-        setPrevNumber("")
-        setOperator("")
-      }
+    if (currentScreenValue === "") return
+
+    if (isOperator(currentScreenValue))
+    {
+      setCurrentScreenValue(buttonValue)
+      return
+    }
+
+    setOperator(buttonValue)
+    setPrevNumber(currentScreenValue)
+    setCurrentScreenValue("")
   }
 
-  function isOperator(value)
+  // 계산기에서 "=" 버튼을 눌렀을 때
+  const eqaulButtonEvent = () =>
   {
-    if (currentButtonValue !== "")
-      {
-        setOperator(value)
-        setPrevNumber(currentButtonValue)
-        setCurrentButtonValue("")
-      }
-      else return
-  }
+    if (currentScreenValue === "" || prevNumber === "" || operator === "") return
 
-  function isClear()
-  {
-    setCurrentButtonValue("")
+    const num1 = parseInt(prevNumber)
+    const num2 = parseInt(currentScreenValue)
+    const result = getCalculatedResult(num1, num2, operator)
+
+    setCurrentScreenValue(result)
     setPrevNumber("")
     setOperator("")
   }
 
-  function isNum(value)
+  // 계산기에서 C 버튼을 눌렀을 때
+  const clearButtonEvent = () =>
   {
-    setCurrentButtonValue(currentButtonValue + value)
+    setCurrentScreenValue("")
+    setPrevNumber("")
+    setOperator("")
   }
 
-  const buttonEvent = (event) =>
+  // 계산기에서 숫자(0~9) 버튼을 눌렀을 때
+  const numberButtonEvent = (event) =>
   {
     const buttonValue = event.target.name
 
-    if (buttonValue === "=")
-    {
-      isEqual()
-    }
-    else if (buttonValue === "+" || buttonValue === "-" || buttonValue === "*" || buttonValue === "/")
-    {
-      isOperator(buttonValue)
-    }
-    else if (buttonValue === "C")
-    {
-      isClear()
-    }
-    else // 숫자
-    {
-      isNum(buttonValue)
-    } 
+    setCurrentScreenValue(currentScreenValue + buttonValue)
   }
 
   return (
     <div className="route">
       <div className="screen_area">
-        <input className="screen_text" readOnly value={currentButtonValue}></input>
+        <input className="screen_text" readOnly value={currentScreenValue}></input>
       </div>
       <div className="button_area">
         <div className="button_line">
-          <button className="button_num" name="7" onClick={buttonEvent}>7</button>
-          <button className="button_num" name="8" onClick={buttonEvent}>8</button>
-          <button className="button_num" name="9" onClick={buttonEvent}>9</button>
-          <button className="button_operator" name="*" onClick={buttonEvent}>*</button>
+          <button className="button_num" name="7" onClick={numberButtonEvent}>7</button>
+          <button className="button_num" name="8" onClick={numberButtonEvent}>8</button>
+          <button className="button_num" name="9" onClick={numberButtonEvent}>9</button>
+          <button className="button_operator" name="*" onClick={operatorButtonEvent}>*</button>
         </div>
         <div className="button_line">
-          <button className="button_num" name="4" onClick={buttonEvent}>4</button>
-          <button className="button_num" name="5" onClick={buttonEvent}>5</button>
-          <button className="button_num" name="6" onClick={buttonEvent}>6</button>
-          <button className="button_operator" name="-" onClick={buttonEvent}>-</button>
+          <button className="button_num" name="4" onClick={numberButtonEvent}>4</button>
+          <button className="button_num" name="5" onClick={numberButtonEvent}>5</button>
+          <button className="button_num" name="6" onClick={numberButtonEvent}>6</button>
+          <button className="button_operator" name="-" onClick={operatorButtonEvent}>-</button>
         </div>
         <div className="button_line">
-          <button className="button_num" name="1" onClick={buttonEvent}>1</button>
-          <button className="button_num" name="2" onClick={buttonEvent}>2</button>
-          <button className="button_num" name="3" onClick={buttonEvent}>3</button>
-          <button className="button_operator" name="+" onClick={buttonEvent}>+</button>
+          <button className="button_num" name="1" onClick={numberButtonEvent}>1</button>
+          <button className="button_num" name="2" onClick={numberButtonEvent}>2</button>
+          <button className="button_num" name="3" onClick={numberButtonEvent}>3</button>
+          <button className="button_operator" name="+" onClick={operatorButtonEvent}>+</button>
         </div>
         <div className="button_line">
-          <button name="C" onClick={buttonEvent}>C</button>
-          <button className="button_num" name="0" onClick={buttonEvent}>0</button>
-          <button className="button_operator" name="/" onClick={buttonEvent}>/</button>
-          <button className="button_equal" name="=" onClick={buttonEvent}>=</button>
+          <button className="button_clear" name="C" onClick={clearButtonEvent}>C</button>
+          <button className="button_num" name="0" onClick={numberButtonEvent}>0</button>
+          <button className="button_operator" name="/" onClick={operatorButtonEvent}>/</button>
+          <button className="button_equal" name="=" onClick={eqaulButtonEvent}>=</button>
         </div>
       </div>
     </div>
